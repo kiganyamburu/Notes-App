4e5b6d36e9efa22e9be1bbbf24157c38f7726fe8
kiganyamburu <mburukiganya@gmail.com>
2025-05-16 05:23:43 +0300

    feat(__init__.py):created a db

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -7,7 +8,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "jfbvjdfvbvfdk"
-    app.config["SQLACHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+
     db.init_app(app)
     
     from .views import views
@@ -16,4 +18,16 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
     
+    from .models import User, Note
+    # from .models import User, Note, Reminder    
+    create_database(app)
+    # create_database(app)
+    
+    
     return app
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+         with app.app_context():
+            db.create_all()
+            print("Created Database!")
--- website/models.py
+++ website/models.py
@@ -13,3 +13,12 @@ class User(db.Model,UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    # Define the relationship with the Note model
+    
+    # class Reminder(db.Model):
+    #     id = db.Column(db.Integer, primary_key=True)
+    #     date = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    #     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #     note_id = db.Column(db.Integer, db.ForeignKey('note.id'))
+    #     # Define the relationship with the Note model
