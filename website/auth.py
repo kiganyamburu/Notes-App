059a910eb469fc828565ce2a0c5330ccf7cc4293
kiganyamburu <mburukiganya@gmail.com>
2025-05-16 05:52:57 +0300

    feat(auth.py):first account has been created

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", boolean=True)
 
 
 @auth.route("/logout")
@@ -17,19 +21,24 @@ def logout():
 def sign_up():
     if request.method == "POST":
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
         if len(email) < 4:
             flash("Email must be greater than 3 characters.", category="error")
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("first name must be greater than 1 characters.", category="error")
         elif password1 != password2:
             flash("Password dont match .", category="error")
         elif len(password1) < 7:
             flash("Password must be at least 7 characters .", category="error")
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            # add user to database
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account created", category="success")
+            return redirect(url_for("views.home"))
         
     return render_template("sign_up.html")
