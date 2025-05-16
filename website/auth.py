849f93fa0e77e8512947a35fb4ccef26253ca483
kiganyamburu <mburukiganya@gmail.com>
2025-05-16 07:06:18 +0300

    done

--- website/auth.py
+++ website/auth.py
@@ -25,7 +25,7 @@ def login():
                 flash("Incorrect password, try again.", category="error")
         else:
             flash("Email does not exist.", category="error")
-    return render_template("login.html", boolean=True)
+    return render_template("login.html",user=current_user)
 
 
 @auth.route("/logout")
@@ -63,4 +63,4 @@ def sign_up():
             flash("Account created", category="success")
             return redirect(url_for("views.home"))
         
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
